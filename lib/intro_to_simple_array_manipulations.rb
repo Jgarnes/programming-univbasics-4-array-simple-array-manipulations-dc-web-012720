3933622b731402de192d18b8171dbd42a3838159
Jeremy Garnes <j06garnes@gmail.com>
2019-12-27 03:30:58 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,27 @@
+def using_push(Array, String)
+  array.push(String)
+end
+
+def using_unshift(Array, String)
+  Array.unshift(String)
+end
+
+def using_pop(Array)
+  Array.pop 
+end   
+
+def pop_with_args(Array)
+  Array.pop(2)
+end   
+
+def using_shift(Array)
+  Array.shift 
+end   
+
+def shift_with_args(Array)
+  Array.shift(2) 
+end   
+
+def using_concat()
+  
+end   
